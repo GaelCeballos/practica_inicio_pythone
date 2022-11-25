2d7b7a25aa0aadc2c4bcbfc946d45bf57f117d3c
Gael Ceballos Nava <gaelceballosnava@gmail.com>
2022-11-24 23:22:03 -0600

    Trabajo del queso

--- index.py
+++ index.py
@@ -1,5 +1,55 @@
-a= int(input("Ingresa un numero: "))
-b= int(input("Ingresa un numero: "))
-c= int(input("Ingresa un numero: "))
-print("El numero menor entre {}, {} y {} es: {}".format(a,b,c,min(a,b,c)))
-print("El número mas grande entre {}, {} y {} es: {}".format(a,b,c, max(a,b,c)))
+titulo = "Bienvenido al test sobre el queso"
+print(titulo + "\n" + "-"*len(titulo)+"\n")
+puntuacion=0
+
+opcion = input("Pregunta 1: Que haces cuando ves una tabla de quesos?\n"
+                                "A- Salgo corriendo \n"
+                                "B- Pruebo uno de los quesos o incluso varios\n"
+                                "C- no puedo evitar comerlos\n")
+
+if opcion == "a":
+    puntuacion += 0
+elif opcion =="b":
+    puntuacion +=5
+elif opcion =="c":
+    puntuacion += 10
+else:
+    print("Las opciones disponibles son a,b y c")
+    exit()
+
+opcion = input("Pregunta 2: ¿Como te gusta la hamburguesa?\n"
+                    "A- Sin queso\n"
+                    "B- con queso\n"
+                    "C- Pan y queso\n ")
+
+if opcion == "a":
+    puntuacion += 0
+elif opcion =="b":
+    puntuacion +=5
+elif opcion =="c":
+    puntuacion += 10
+else:
+    print("Las opciones disponibles son a,b y c")
+    exit()
+
+opcion = input("Pregunta 3: ¿Eres intolerante a la lactosa?\n"
+                        "A- si\n"
+                        "B- A veces\n"
+                        "C- No\n")
+
+if opcion == "a":
+    puntuacion += 0
+elif opcion =="b":
+    puntuacion +=5
+elif opcion =="c":
+    puntuacion += 10
+else:
+    print("Las opciones disponibles son a,b y c")
+    exit()
+
+if puntuacion >=25:
+    print("Eres fanatico del queso")
+elif puntuacion >=15:
+    print("Te gusta el queso")
+else:
+    print("No te gusta el queso")
